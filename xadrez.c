020a0f4178a6195d50f0cdc32062b01b740cc5bb
Filipe Kleber <146716868+Filipekleber@users.noreply.github.com>
2025-03-19 17:55:45 +0000

    Desafio do nivel novato do xadrez

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,156 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+    
+        char opcao, opcaoTorre, opcaoBispo, opcaoRainha;
+        int movimentacao = 1;
+    
+        printf("Escolha uma peça para movimentar\n");
+        printf("T. Torre\n");
+        printf("B. Bispo\n");
+        printf("R. Rainha\n");
+        printf("Escolha uma da opções: ");
+        scanf(" %c", &opcao);
+     
+    switch (opcao)
+    {
+    
+    //Função da torre;
+    //------------------------------------------------------------------------------------
+    case 'T':
+    case 't':
+    
+    //Escolhendo se vai para horizontal ou para frontal;
+    
+        
+        printf("Escolha para onde ele vai?\n");
+        printf("D. Direita\n");
+        printf("E. Esquerda\n");
+        printf("Escolha uma das opcoes: ");
+        scanf(" %c", &opcaoTorre);
+        
+    //----------------------------------------------------------------------------------
+    
+        switch (opcaoTorre)
+        {
+        case 'D':
+        case 'd':
+    
+        while (movimentacao <= 5)
+        {
+            printf("Direita\n");
+            movimentacao++;
+        }
+        
+        break;
+    
+    //----------------------------------------------------------------------------------
+    
+        case 'E':
+        case 'e':
+    
+        while (movimentacao <= 5)
+        {
+            printf("Esquerda\n");
+            movimentacao++;
+        }
+        break;
+    
+        default:
+        printf("Opcao invalida\n");
+            break;
+        }
+    //Fim do comando da Torre;
+    //--------------------------------------------------------------------------------------
+    
+    case 'B':
+    case 'b':
+    
+        printf("Escolha para qual diagonal seu bisto vai?\n");
+        printf("D. Diagonal Direita\n");
+        printf("E. Diagonal Esquerda\n");
+        printf("Escolha uma da opções: ");
+        scanf(" %c", &opcaoBispo);
+    
+    switch (opcaoBispo)
+    {
+    case 'D':
+    case 'd':
+        do
+        {
+            printf("Diagonal Direita\n");
+            movimentacao++;
+        } while (movimentacao <= 5);
+        
+        break;
+    
+    //----------------------------------------------------------------------------------------
+    
+    case 'E':
+    case 'e':
+        do
+        {
+            printf("Diagonal Esquerdo\n");
+            movimentacao++;
+        } while (movimentacao <= 5);
+        
+    break;
+    
+    default:
+        printf("Opcao invalida\n");
+        break;
+    }
+    
+    break;
+    //FIM DO COMANDO BISPO
+    //---------------------------------------------------------------------------------------------
+    
+    case 'R':
+    case 'r':
+    
+        printf("Para qual lado voce quer mover a rainha?\n");
+        printf("D. Direito\n");
+        printf("E. Esquerdo\n");
+        printf("Escolha uma da opções: ");
+        scanf(" %c", &opcaoRainha);
+    
+        switch (opcaoRainha)
+        {
+        case 'D':
+        case 'd':
+    
+            for(movimentacao; movimentacao <= 8; movimentacao++)
+            {
+                printf("Direita\n");
+            }
+            
+            break;
+    
+    //--------------------------------------------------------------------------------------------
+    
+            case 'E':
+            case 'e':
+    
+            for(movimentacao; movimentacao <= 8; movimentacao++)
+            {
+                printf("Esquerda\n");
+            }
+            
+            break;
+        
+        default:
+            printf("Opcao invalida\n");
+            break;
+        }
+    
+    break;
+    //FIM DO COMANDO RAINHA
+    //-----------------------------------------------------------------------------------------------
+    
+    default:
+            printf("Opcao invalida\n");
+        break;
+    }
+    
     return 0;
 }
